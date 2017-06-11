e59eefdeec7907e695158429020d143482e23685
Thomas Porcaro <thomas.porcaro@gmail.com>
2017-06-11 20:25:51 +1000

    added rolling mean line to visual method

--- coinanalysis/visual.py
+++ coinanalysis/visual.py
@@ -3,8 +3,12 @@
 import matplotlib.pyplot as plt
 
 
-def view_price_time_series(market):
-    market.history.plot(x="TimeStamp", y="Price")
+def view_price_time_series(market, rolling_mean_window=3):
+    history = market.history
+    history = history.assign(
+        PriceRollingMean=history["Price"].rolling(window=rolling_mean_window, center=False).mean()
+    )
+    history.plot(x="TimeStamp", y=["PriceRollingMean","Price"])
     plt.title(market.name)
     plt.xlabel("Time")
     plt.ylabel("Price [{:s}]".format(market.basis))
